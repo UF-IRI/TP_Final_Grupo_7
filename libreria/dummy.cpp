6f491ef56d42ee92f2f7251a35f6a9f9a0056448
agostina nobo <agosnobo@yahoo.com>
2022-11-11 08:54:52 -0300

    dudas

---
+++ libreria/dummy.cpp
@@ -0,0 +1,11 @@
+/*
+DUDAS:
+-hay que eliminar los pacientes que archivamos?
+	-sino, hay que crear un archivo de archivados? que datos le pasamos a ese archivo?
+-hacer lista general o 5 listas?
+-ultimo paso es armar la lista de secretaria o hay que hacer la funcion llamar?
+
+
+
+
+*/
