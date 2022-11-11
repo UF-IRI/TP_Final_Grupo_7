09a1242f91384f35e674dab51a11d04634fadaaa
agostina nobo <agosnobo@yahoo.com>
2022-11-11 13:48:09 -0300

    carpetitas dev

---
+++ libreria/agos.cpp
@@ -0,0 +1,57 @@
+//PROBANDO
+
+#include "libreria.h"
+
+time_t PasarAFecha(string dato)
+{
+	int i = 0;
+	int day, month, year;
+	int cont = 0;
+	string auxD, auxM, auxY;
+
+	while (dato[i] != '\0')
+	{
+		int k = 0;
+		while (dato[i] != '/')
+		{
+
+			switch (cont)
+			{
+			case 0:
+			{
+				auxD[k] = dato[i];
+				break;
+			}
+			case 1:
+			{
+				auxM[k] = dato[i];
+				break;
+			}
+			case 2:
+			{
+				auxY[k] = dato[i];
+				break;
+			}
+			}
+			k++;
+			i++;
+
+		}
+		cont++;
+		i++;
+	}
+	day = stoi(auxD);
+	month = stoi(auxM);
+	year = stoi(auxY);
+
+	/*
+	tm date{};
+	date.tm_year = year;
+	date.tm_mon = month;
+	date.tm_mday = day;
+
+	time_t finalDate = mktime(&date);
+	*/
+
+	return finalDate;
+}
