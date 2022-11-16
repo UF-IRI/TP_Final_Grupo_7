7fc6c8d143a8b8eaa4b49498c7dc2639282e5b42
alma marquez <almamarquez2003@gmail.com>
2022-11-16 00:09:41 -0300

    funcionidmedico

--- libreria/almi.cpp
+++ libreria/almi.cpp
@@ -61,7 +61,8 @@ secretaryList convertToSecretary(pacient aux, string nameFileAppointment, string
 
 	long unsigned int dniaux = 0;
 	
-	int iddocaux = 0;
+	string iddocaux;
+	readapp >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy;//header
 
 	while (readapp) //leo el archivo de appointment
 	{
@@ -79,7 +80,7 @@ secretaryList convertToSecretary(pacient aux, string nameFileAppointment, string
 	}
 
 	string cellphoneaux;
-
+	readcon >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy;//header
 	while (readcon) //leo el de contactos
 	{
 		readcon >> dniaux >> coma >> cellphoneaux >> coma >> dummy >> coma >> dummy >> coma >> dummy;
@@ -101,3 +102,43 @@ secretaryList convertToSecretary(pacient aux, string nameFileAppointment, string
 	return auxsec;
 
 }
+
+bool doctorIdList(string nameFileAppointment, string** list, int* sizeList)
+{
+	fstream readapp;
+
+	readapp.open(nameFileAppointment, ios::in);
+	if (!(readapp.is_open())) return false;
+
+	char coma;
+	string dummy;
+
+	readapp >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy;//header
+
+	appointment aux;
+
+	while (readapp)
+	{
+		readapp >> aux.dniPacient >> coma >> aux.dateAppointment >> coma >> aux.dateAppointment >> coma >> aux.asistance >> coma >> aux.idDoctor;
+		for (int i = 0; i < *sizeList:i++)
+		{
+			if (**list[i] == aux.idDoctor)
+			{
+				break;
+			}
+		}
+		if (i == *sizeList)
+		{
+			string* newdoc = new string[*sizeList + 1];
+			newdoc[*sizeList] = aux.idDoctor;
+		}
+	}
+	readapp.close();
+	*sizeList = *sizeList + 1;
+	delete* list;
+	*list = newdoc;
+	return true;
+}
+
+}
+
