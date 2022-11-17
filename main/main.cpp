91f180278190186d58bfa44c96231eb0b752db54
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2022-11-17 20:33:44 -0300

    main cambios

--- main/main.cpp
+++ main/main.cpp
@@ -2,20 +2,20 @@
 #include "libreria.h"
 #include "pochi.cpp"
 #include "loren.cpp"
-#include "agos.cpp"
-#include "almi.cpp"
+//#include "agos.cpp"
+//#include "almi.cpp"
 
 int main() {
 
 	string contactFile = "IRI_Contactos.csv";
 	string appointmentFile = "IRI_Consultas.csv";
+	string pacientFile = "IRI_Pacientes.csv";
 	int appointmentSize=0;
 	appointment* listAppointment = new appointment[appointmentSize];
 	readAppointment(appointmentFile, &appointmentSize, listAppointment);
-
+	
 	int unrecoverableSize = 0;
 	pacient* listPacientUnrecoverable = new pacient[unrecoverableSize];
-	string pacientFile = "IRI_Pacientes.csv";
-	//readPacients( pacientFile, &unrecoverableSize, listPacientUnrecoverable, appointmentSize, listAppointment);
-
+	readPacients( pacientFile, &unrecoverableSize, listPacientUnrecoverable, appointmentSize, listAppointment, pacientFile);
+	
 }
