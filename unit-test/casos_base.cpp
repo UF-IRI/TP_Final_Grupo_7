0888aa1156118cf961dbad2abb731745360b3a45
agostina nobo <agosnobo@yahoo.com>
2022-11-17 22:43:07 -0300

    ANDUVO UN UNIT TEST

--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -6,12 +6,13 @@
 //bool insuranceList(string nameFilePacient, string** list, int*sizeList); //genera array de obras sociales
 //void newFile(string SecretaryFileName, secretaryList*& listPacient, int size); //recibir un array de secretaria
 //void secretary(string SecretaryFileName, int sizeFile); 
+//time_t convertDate(string dato);
 
 
 namespace Casos_Base::tests {
 	TEST(findContact, caso1) {
 		//LINEA 69 ARCHIVO CONTACTOS: 859052988,+86 163 839 6359,+48 621 670 1298,Barby,aplayer1v@theatlantic.com
-		string contactFile = "C:\\Users\\agosn\\Source\\Repos\\TP_Final_Grupo_7\\data_files\\input\\IRI_Contactos.csv";
+		string contactFile = "IRI_Contactos.csv";
 		contact aux1;
 		aux1.dniContact = 859052988;
 		aux1.adress = "Barby";
@@ -21,17 +22,26 @@ namespace Casos_Base::tests {
 		contact aux2;
 		bool aver=findContact(contactFile, &aux2, aux1.dniContact);
 		EXPECT_FALSE(aver);
+		
+		/*
 		EXPECT_THAT(aux2.dniContact, aux1.dniContact);
 		EXPECT_THAT(aux2.adress, aux1.adress);
 		EXPECT_THAT(aux2.mail, aux1.mail);
 		EXPECT_THAT(aux2.numberPhone, aux1.numberPhone);
 		EXPECT_THAT(aux2.numberTelephone, aux1.numberTelephone);
+		*/
 	}
-	/*
-	TEST(Casos_Base, Test_Caso2) {
-		ASSERT_THAT(1, 1);
+	TEST(convertDate, test1) {
+		string hoy="17/11/2022";
+		tm fecha{};
+		fecha.tm_year = 2022 - 1900;
+		fecha.tm_mon = 11 - 1;
+		fecha.tm_mday = 17;
+		time_t esperada = mktime(&fecha);
+		time_t real = convertDate(hoy);
 	}
 
+	/*
 	TEST(Casos_Base, Test_Caso3) {
 		ASSERT_THAT(1, 1);
 	}
