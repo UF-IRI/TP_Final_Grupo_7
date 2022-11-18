4f8e6a20bafa9e1d1671f6e7d0e21173a61442a1
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2022-11-17 23:28:30 -0300

    commit por las dudas

--- main/main.cpp
+++ main/main.cpp
@@ -5,10 +5,9 @@
 int main() 
 {
 
-	string contactFile = "C:\\Users\\Mariano\\OneDrive\\Documentos\\Lorenzo\\Universidad\\1er año\\IRI\\Tp final\\data_files\\input\\IRI_Contactos.csv";
+	string contactFile = "IRI_Contactos.csv";
 	string appointmentFile = "IRI_Consultas.csv";
-	
-	string pacientFile = "C:\\Users\\Mariano\\OneDrive\\Documentos\\Lorenzo\\Universidad\\1er año\\IRI\\Tp final\\data_files\\input\\IRI_Pacientes.csv";
+	string pacientFile = "IRI_Pacientes.csv";
 	string filePacientUnrecoverable = "C:\\Users\\Mariano\\OneDrive\\Documentos\\Lorenzo\\Universidad\\1er año\\IRI\\Tp final\\data_files\\output\\IRI_Irrecuperables.csv";
 	string filePacientRecoverable = "C:\\Users\\Mariano\\OneDrive\\Documentos\\Lorenzo\\Universidad\\1er año\\IRI\\Tp final\\data_files\\output\\IRI_Recuperables.csv";
 	int appointmentSize=0;
